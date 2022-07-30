d0ef9633e3516820d4d33fbbdad15db6c8a22fdd
TheolaAshu <ashutheola@gmail.com>
2022-07-30 15:55:21 -0700

    working with containers

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,39 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text('Bright like a diamond'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(
-           image: AssetImage('images/Diamond.jpg'),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+               Container(
+              height: 100.0,
+              width: 100.0,
+              margin: EdgeInsets.only(left: 30.0),
+              padding: EdgeInsets.all(20.0),
+              color: Colors.white,
+              child: Text('Box1'),
+             ),
+             
+              Container(
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.only(left: 30.0),
+                padding: EdgeInsets.all(20.0),
+                color: Colors.blue,
+                child: Text('Box2'),
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                margin: EdgeInsets.only(left: 30.0),
+                padding: EdgeInsets.all(20.0),
+                color: Colors.red,
+                child: Text('Box3'),
+              ),
+            ],
           ),
         ),
       ),
     ),
   );
 }
-
