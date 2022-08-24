178b009d4c5f76034c97fd583cd79d2fce5586e5
TheolaAshu <ashutheola@gmail.com>
2022-08-23 22:03:19 -0700

    updating fonts and adjusting the pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,78 @@ void main() {
         body: SafeArea(
           child: Column(
             children: <Widget>[
-               Container(
-              height: 100.0,
-              width: 100.0,
-              margin: EdgeInsets.only(left: 30.0),
-              padding: EdgeInsets.all(20.0),
-              color: Colors.white,
-              child: Text('Box1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Diamond.jpg'),
+              ),
+              Text(
+                'Codegod',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),  
              ),
-             
-              Container(
-                height: 100.0,
-                width: 100.0,
-                margin: EdgeInsets.only(left: 30.0),
-                padding: EdgeInsets.all(20.0),
-                color: Colors.blue,
-                child: Text('Box2'),
+             Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                 Icon(
+                  Icons.phone,
+                  size: 20.0,
+                  color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('+237 123 456 78',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'Source Sana Pro',
+                    fontSize: 20.0,
+                  ),
+                  ),
+                ]
+                ),
+             ),
+              Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: EdgeInsets.all(25.0),
+              child:Row(
+                children: <Widget>[
+                 Icon(
+                  Icons.email,
+                  size: 20.0,
+                  color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('codegod@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'Source Sana Pro',
+                    fontSize: 20.0,
+                  ),
+                  ),
+                ]
+                ),
               ),
-              Container(
-                height: 100.0,
-                width: double.infinity,
-                margin: EdgeInsets.only(left: 30.0),
-                padding: EdgeInsets.all(20.0),
-                color: Colors.red,
-                child: Text('Box3'),
-              ),
+             )
             ],
           ),
         ),
